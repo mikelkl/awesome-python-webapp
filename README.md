ffc8b22fb32aff1ff61467fd8d4e92bff81a9c34
Mike Liu <mikelkl@foxmail.com>
2015-12-12 09:42:36 +0800

    version 1

---
+++ README.md
@@ -0,0 +1,1 @@
+# 学习Python的第一个Blog网站，后端实现了自己编写ORM, Model, MVC, REST API等功能， 前端使用了uikit框架, Vue.js框架
---
+++ www/config_default.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+'''
+Default configurations.
+'''
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www-data',
+        'password': 'www-data',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
